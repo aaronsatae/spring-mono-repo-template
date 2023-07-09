6024c47772a51f91038ef047615cf3d5396c4fd3
Jihoon Park <chad9349@gmail.com>
2023-07-09 14:03:53 +0900

    initial commit

---
+++ src/main/java/com/example/springmonorepotemplate/SpringMonorepoTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.example.springmonorepotemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMonorepoTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMonorepoTemplateApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springmonorepotemplate/SpringMonorepoTemplateApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springmonorepotemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMonorepoTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
